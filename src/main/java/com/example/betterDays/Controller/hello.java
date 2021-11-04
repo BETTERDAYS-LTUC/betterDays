13088394aa0be388f570c4d78a4bbde0f8104739
Qutadah95 <qutadaalmomani@yahoo.com>
2021-11-04 12:52:14 +0200

    gg

---
+++ src/main/java/com/example/betterDays/Controller/hello.java
@@ -0,0 +1,20 @@
+package com.example.betterDays.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class hello {
+    @RequestMapping({"/hello"})
+    @ResponseBody
+    public String hello() {
+        System.out.println("hello world");
+        return "Hello World";
+    }
+    @GetMapping("/")
+    public String getHello(){
+        return "hello";
+    }
+}
