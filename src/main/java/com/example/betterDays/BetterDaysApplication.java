fb127e4db724ecf7b6f02deed0f05bc745b318a3
abdallahhamoury <abdallah.hamoury@gmail.com>
2021-11-04 11:13:24 +0200

    update

---
+++ src/main/java/com/example/betterDays/BetterDaysApplication.java
@@ -0,0 +1,13 @@
+package com.example.betterDays;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BetterDaysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BetterDaysApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/betterDays/BetterDaysApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.betterDays;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BetterDaysApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
