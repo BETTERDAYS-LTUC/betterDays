0d8e336026a7ce9796b5e208568f7f8e477dfaa3
maiada-ibrahim <maiada.ibrahim.27@icloud.com>
2021-11-09 01:56:01 +0200

    login

--- src/main/java/com/example/betterDays/Security/UserDetailsServiceImpl.java
+++ src/main/java/com/example/betterDays/Security/UserDetailsServiceImpl.java
@@ -1,6 +1,8 @@
 package com.example.betterDays.Security;
 
+import com.example.betterDays.Entities.DoctorEntity;
 import com.example.betterDays.Entities.Patient;
+import com.example.betterDays.Repositories.DoctorRepository;
 import com.example.betterDays.Repositories.PatientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -12,9 +14,19 @@ import org.springframework.stereotype.Service;
 public class UserDetailsServiceImpl implements UserDetailsService {
     @Autowired
     PatientRepository patientRepository;
+    @Autowired
+    DoctorRepository doctorRepository;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return patientRepository.findByUserName(username);
+        DoctorEntity doctor = doctorRepository.findByUsername(username);
+        Patient patient=patientRepository.findByUsername(username);
+        if (doctor == null && patient == null ) {
+            throw new UsernameNotFoundException("Dose not exist");
+        }else if(patient != null){
+            return patient;
+        }else{
+            return doctor;
+        }
     }
 }
